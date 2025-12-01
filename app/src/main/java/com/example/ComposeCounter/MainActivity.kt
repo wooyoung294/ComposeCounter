36cf5db40c9d9264c174b270f6490cb5f0ab40d4
wooyoung.choi <160103260+wooyoung294@users.noreply.github.com>
2025-12-01 23:20:13 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/ComposeCounter/MainActivity.kt
+++ app/src/main/java/com/example/ComposeCounter/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                 Box(
                     Modifier
                         .fillMaxSize()
-                        .semantics { testTagsAsResourceId = true } // ← testTag를 resource-id로 노출
+                        .semantics { testTagsAsResourceId = false } // ← testTag를 resource-id로 노출
                 ) {
                     CounterScreen(Modifier.align(Alignment.Center))
                 }
